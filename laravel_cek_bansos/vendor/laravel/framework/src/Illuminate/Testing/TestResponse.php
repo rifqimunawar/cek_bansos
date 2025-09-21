457b4bff3c4845464224e61bd40de8b41857f2f0
Rifqi Munawar <rifqimunawar48@gmail.com>
2025-09-21 09:38:53 +0700

    refactor code

--- laravel_cek_bansos/vendor/laravel/framework/src/Illuminate/Testing/TestResponse.php
+++ laravel_cek_bansos/vendor/laravel/framework/src/Illuminate/Testing/TestResponse.php
@@ -246,6 +246,37 @@ class TestResponse implements ArrayAccess
     }
 
     /**
+     * Assert whether the response is redirecting back to the previous location with the given errors in the session.
+     *
+     * @param  string|array  $keys
+     * @param  mixed  $format
+     * @param  string  $errorBag
+     * @return $this
+     */
+    public function assertRedirectBackWithErrors($keys = [], $format = null, $errorBag = 'default')
+    {
+        $this->assertRedirectBack();
+
+        $this->assertSessionHasErrors($keys, $format, $errorBag);
+
+        return $this;
+    }
+
+    /**
+     * Assert whether the response is redirecting back to the previous location with no errors in the session.
+     *
+     * @return $this
+     */
+    public function assertRedirectBackWithoutErrors()
+    {
+        $this->assertRedirectBack();
+
+        $this->assertSessionHasNoErrors();
+
+        return $this;
+    }
+
+    /**
      * Assert whether the response is redirecting to a given route.
      *
      * @param  \BackedEnum|string  $name
@@ -1670,6 +1701,8 @@ class TestResponse implements ArrayAccess
             foreach ($key as $value) {
                 $this->assertSessionMissing($value);
             }
+
+            return $this;
         }
 
         if (is_null($value)) {
@@ -1724,7 +1757,7 @@ class TestResponse implements ArrayAccess
     {
         $content = $this->content();
 
-        if (function_exists('json_validate') && json_validate($content)) {
+        if (json_validate($content)) {
             $this->ddJson($key);
         }
 
@@ -1735,6 +1768,7 @@ class TestResponse implements ArrayAccess
      * Dump the JSON payload from the response and end the script.
      *
      * @param  string|null  $key
+     * @return never
      */
     public function ddJson($key = null)
     {
